59e69a39c384801abce6c2785c966648abad513d
Gray <graysitory@users.noreply.github.com>
2019-12-20 15:24:16 -0800

    Initial version of Vision Comma Cleaner

---
+++ Vision Comma Cleaner.jsx
@@ -0,0 +1,106 @@
+﻿
+
+var myVersion = "0.5";
+var myName = "Vision Comma Cleaner - Version " + myVersion;
+var myDescription = "Removes the comma between project name and \"City, ST\" from resumes exported from Vision.";
+var myExampleFrom = "UC Berkeley, Li Ka Shing Center, Berkeley, CA";
+var myExampleTo = "UC Berkeley, Li Ka Shing Center Berkeley, CA"
+var myNotes = "Highlight the text you want to clean up and run this script. \r  - Only useful on project lists exported from Vision\u2014it might make commas disappear! \r  - International locations (Kashiwa, Japan) should be handled manually."
+var grepFindString = {findWhat: "(,)( \\w+)?( )(\\w+, \\w\\w(\\*)?$)"};
+var grepChangeString = {changeTo: "$2 $4"};
+
+
+
+
+function myChangeGrep(mySelection) {
+   
+             // http://jongware.mit.edu/idcs6js/pc_Application.html#changeGrep
+            
+            // clear find preferences
+            app.findGrepPreferences = NothingEnum.nothing;
+            app.changeGrepPreferences = NothingEnum.nothing;
+            
+            
+            // set find preferences
+            app.findGrepPreferences.properties = grepFindString; 
+            app.changeGrepPreferences.properties = grepChangeString;
+            
+            // Run grep find/change on selection
+            mySelection.changeGrep();    
+            
+            // clear find preferences
+            app.findGrepPreferences = NothingEnum.nothing;
+            app.changeGrepPreferences = NothingEnum.nothing;
+    
+    }
+
+
+
+function buildWindow(myWindow) {
+
+      var myPreferredSize = [600,50];
+        
+               myWindow.preferredSize = myPreferredSize;
+               myWindow.margins = 20;
+               myWindow.alignChildren = "fill";
+               
+     // panel describing script
+     var myDescriptionPanel = myWindow.add("panel", undefined, "Description");
+            myDescriptionPanel.add("statictext", undefined, myDescription);
+            myDescriptionPanel.preferredSize = myPreferredSize;
+            myDescriptionPanel.alignChildren = "left";
+      
+      // panel giving an example of the changes
+      var myExamplePanel = myWindow.add("panel", undefined, "Changes:");
+            myExamplePanel.preferredSize = myPreferredSize;
+            myExamplePanel.alignChildren = "left";
+            
+        var myExamplePanelExamples = myExamplePanel.add("group", undefined);
+               myExamplePanelExamples.margins = [0,8,0,8]; // left, top, right, bottom
+                
+                var myExampleFromPanel = myExamplePanelExamples.add("panel", undefined, "From");
+                       myExampleFromPanel.add("statictext", undefined, myExampleFrom);
+                var myExampleToPanel = myExamplePanelExamples.add("panel", undefined, "To");
+                       myExampleToPanel.add("statictext", undefined, myExampleTo);
+         
+     // panel with notes/instructions    
+      var myNotesPanel = myWindow.add("panel", undefined, "Notes")
+             var myNotesPanelText = myNotesPanel.add("statictext", undefined, myNotes, {multiline: true});
+                    myNotesPanelText.preferredSize = myPreferredSize;
+                        
+                        
+       // ok, cancel buttons
+        var myActionGroup = myWindow.add("group", undefined);
+                myActionGroup.alignment = "right";
+                
+                var myCancelBtn = myActionGroup.add("button", undefined, "Cancel"); // auto responds to Esc key
+                var myOkBtn = myActionGroup.add("button", undefined, "Run", {name: "ok"}); // auto responds to Enter key
+    
+    }
+
+function main() {
+     app.scriptPreferences.userInteractionLevel = UserInteractionLevels.INTERACT_WITH_ALL; // allow user interaction with dialogs
+    
+     var mySelection = app.selection[0];  // get selected text
+    
+    
+    var myWindow = new Window("dialog", myName); // create dialog window
+            // resizable window:  var myWindow = new Window("dialog", myName, undefined, {resizeable: true}); // create dialog window
+
+        buildWindow(myWindow); // build dialog contents with function
+
+
+       if (myWindow.show() == true) { // if the dialog window is showing (ok is clicked)
+           
+           if ((mySelection == "") || (mySelection == undefined)) {
+               alert("Please select the text you want to clean up, and run this script again.")
+               } else {
+                myChangeGrep(mySelection);
+            }
+    
+        }
+    
+    }
+
+
+main();
---
+++ indesign-grep-panel.js
@@ -0,0 +1,107 @@
+﻿
+// TODO: highlight line endings that don't have city, ST
+
+var version = 1.0;
+
+var actionArr = new Array();
+
+// GREP strings: refrence name, friendly name, description, example, grep string, default value
+var grepAction1 = ["grep1", "Grep Action 1", "Removes commas between project name and city, state as exported by Vision.", "...Li Ka Shing Center, Berkeley, CA ==> ...Li Ka Shing Center Berkeley, CA", "grepString", false];
+
+var grepAction2 = ["grep2", "Grep Action 2", "Does something else", "example", "grep string", true];
+
+actionArr.push(grepAction1);
+
+
+function autoGetMyInput() { // attempt to allow for looping through an array of settings and defining a checkbox for each. Not sure how to implement without declaring a unique var for each checbox.
+    
+    var myWindow = new Window("dialog", "Text Cleanup - version " + version);
+            myWindow.preferredSize = [500, 500];
+            
+            var myActionsPanel = myWindow.add("panel", undefined, "Cleanup Actions");
+                myActionsPanel.orientation = "column";
+                myActionsPanel.alignChildren = "left";
+                myActionsPanel.margins = 20;
+                myActionsPanel.preferredSize = [420,500];
+            
+            
+            for (var i = 0; i < actionArr.length; i++) { // loop through actionArr
+                
+                
+                for (var j = 0; j < actionArr[i].length; j++) { // loop through each arr in actionArr
+                    
+                            $.writeln(actionArr[i][j]); // print each element in each array of actionArr
+                    
+                    }
+                
+                }
+            
+            
+            myWindow.show();
+    
+    }
+
+
+
+
+
+
+
+function getMyInput() {
+        
+        var myWindow = new Window("dialog", "Text Cleanup - version " + version);
+            myWindow.preferredSize = [450,600]
+            
+         var myChecklistPanel = myWindow.add("panel", undefined, "Cleanup Actions");
+                myChecklistPanel.orientation = "column";
+                myChecklistPanel.alignChildren = "left";
+                myChecklistPanel.margins = 20;
+                myChecklistPanel.preferredSize = [420,500];
+                
+                function buildCheckbox(panel, arr) {
+                    
+                    
+                    var checklistGroup = panel.add("group", undefined);
+                            checklistGroup.orientation = "column";
+                    
+                    var checklistItem = checklistGroup.add("checkbox", undefined, arr[1]);
+                            checklistItem.margins = 0;
+                            var descriptionGroup = panel.add("group", undefined);
+                             var description = descriptionGroup.add("statictext", undefined, arr[2]);
+                            descriptionGroup.add("statictext", undefined, "EXAMPLE: " + arr[3]);
+                            checklistItem.value = arr[5];
+                            
+                    
+                    checklistItem.margins = 0;
+                    
+                    }
+                
+               var grepAction4 = buildCheckbox(myChecklistPanel, grepAction1);
+         
+         
+            var myGrepAction1 = myChecklistPanel.add("checkbox", undefined, grepAction1[1]);
+            myGrepAction1.value = grepAction1[5];
+            myChecklistPanel.add("statictext", undefined, grepAction1[2]);
+            
+            var myGrepAction2 = myChecklistPanel.add("checkbox", undefined, grepAction2[1]);
+            myGrepAction2.value = grepAction2[5];
+            
+            
+            
+         var myActionBtnGroup = myWindow.add("group");
+            var myCancelBtn = myActionBtnGroup.add("button", undefined, "Cancel");
+            var myOkBtn = myActionBtnGroup.add("button", undefined, "Ok");
+            
+          myOkBtn.onClick = function() {
+              
+             $.writeln("ok clicked");
+             exit();
+              }
+    
+        myWindow.show();
+    
+    }
+
+// getMyInput();
+
+getMyInput()
